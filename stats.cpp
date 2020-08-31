754581cdb109bd3fa2a4416ff2f05ca668011fe6
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-08-31 22:40:02 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,8 +3,13 @@ using namespace Statistics;
 Stats Statistics::ComputeStatistics(const std::vector<float>& n) {
 	Stats s;
 	s.computeAverage(n);
-	s.computeMax(n);
-	s.computeMin(n);
+	if(n.size()==0)
+	    s.min=s.max=(0.0/0.0);
+	else
+	{
+	    s.computeMax(n);
+	    s.computeMin(n);
+	}
 	return s;
     
 }
