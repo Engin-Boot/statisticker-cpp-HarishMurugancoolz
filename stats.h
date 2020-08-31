e2dacee647aea511ae07551089d64ba5d18d996e
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-08-31 21:37:12 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,49 @@
 #include <vector>
+#include<cmath>
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+	class Stats
+	{
+	public:
+		float sum = 0;
+		float average;
+		float max=0;
+	    float min=1000000000.0;
+
+	
+		float getAverage(const std::vector<float>& n)
+		{
+		    for (int i = 0; i < n.size(); i++)
+			{
+				sum += n[i];
+			}
+			return(sum/n.size());
+		}
+		float getMax(const std::vector<float>& n)
+		{
+		    for (int i = 0; i < n.size(); i++)
+			{
+				if (max < n[i])
+				{
+					max = n[i];
+				}
+			}
+			return(max);
+		}
+		float getMin(const std::vector<float>& n)
+		{
+		    for (int i = 0; i < n.size(); i++)
+			{
+				if (min < n[i])
+				{
+					min = n[i];
+				}
+			}
+			return(min);
+		}
+		
+    };
+	    
+Stats ComputeStatistics(const std::vector<float>& n);
 }
+
