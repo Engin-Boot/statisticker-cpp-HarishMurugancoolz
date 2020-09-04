3b4d9e200df048cd9dfff7f9f5563b56a3cd3398
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-04 19:59:49 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -4,6 +4,7 @@
 #include "stats.h"
 
 #include <cmath>
+using namespace std;
 
 TEST_CASE("reports average, minimum and maximum") {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
@@ -23,4 +24,14 @@ TEST_CASE("average is NaN for empty array"){
     
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
+}
+TEST_CASE("reports average,minimum,maximum for input containing NaN values")
+{
+    auto computedStats = Statistics::ComputeStatistics({nan(""),1.5,7.5,3.9,20.8});
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedStats.average - 8.175) < epsilon);
+    REQUIRE(std::abs(computedStats.max - 20.8) < epsilon);
+    REQUIRE(std::abs(computedStats.min - 0.5) < epsilon);
+    
+    
 }
