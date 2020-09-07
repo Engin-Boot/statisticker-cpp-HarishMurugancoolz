c013462505470e5569213f189ed5b19bd38984d7
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-07 20:10:53 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 using namespace std;
 
-TEST_CASE("reports average, minimum and maximum") {
+TEST_CASE("When the input array are of real numbers then report average ,maximum and minimum ") {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
@@ -14,20 +14,27 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
-TEST_CASE("average,minimum,maximum is NaN for empty array"){
+TEST_CASE("when the input array is empty then reports NaN for average,maximum,minimum"){
     auto computedStats = Statistics::ComputeStatistics({});
     REQUIRE(std::isnan(computedStats.average)==true);
     REQUIRE(std::isnan(computedStats.max)==true);
     REQUIRE(std::isnan(computedStats.min)==true);
     
 }
-TEST_CASE("reports average,minimum,maximum for input containing NaN values")
+TEST_CASE("when the input array contains NaN value then reports average,minimum,maximum of non NaN values")
 {
     auto computedStats = Statistics::ComputeStatistics({nan(""),0.5,7.5,3.9,20.8});
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.average - 8.175) < epsilon);
     REQUIRE(std::abs(computedStats.max - 20.8) < epsilon);
     REQUIRE(std::abs(computedStats.min - 0.5) < epsilon);
-    
+}
+
+TEST_CASE("when the input array contains only NaN value then reports NaN for average,minimum,maximum")
+{
+    auto computedStats = Statistics::ComputeStatistics({nan(""),nan(""),nan("")});
+    REQUIRE(std::isnan(computedStats.average)==true);
+    REQUIRE(std::isnan(computedStats.max)==true);
+    REQUIRE(std::isnan(computedStats.min)==true);
     
 }
